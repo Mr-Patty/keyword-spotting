2a94da865ffe44e541efd68a25d4ffca65eb8732
Oleg Shevchenko <shevchenko@ap-team.ru>
2021-11-03 01:30:55 +0300

    choose output name for onnx model

--- convert2onnx.py
+++ convert2onnx.py
@@ -7,6 +7,7 @@ from model import SpeechResModel
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--checkpoint', default='models/model_v4.pt', help='Path to saved checkpoint')
+    parser.add_argument('--output', default='models/model_v4.onnx', help='Path where to save onnx model')
     parser.add_argument('--labels_set', default=['down', 'go', 'left', 'no', 'off', 'on', 'right', 'stop', 'up', 'yes'],
                         help='Set of labels')
     namespace = parser.parse_args()
@@ -14,6 +15,7 @@ if __name__ == '__main__':
 
     torch_checkpoint = argv['checkpoint']
     labels_set = argv['labels_set']
+    output = argv['output']
     model = SpeechResModel(n_labels=len(labels_set)+2).float()
 
     model.load_state_dict(torch.load(torch_checkpoint))
@@ -27,14 +29,14 @@ if __name__ == '__main__':
     # Export the model
     torch.onnx.export(model,  # model being run
                       x,  # model input (or a tuple for multiple inputs)
-                      "key-spotting.onnx",  # where to save the model (can be a file or file-like object)
+                      output,  # where to save the model (can be a file or file-like object)
                       export_params=True,  # store the trained parameter weights inside the model file
                       opset_version=10,  # the ONNX version to export the model to
                       do_constant_folding=True,  # whether to execute constant folding for optimization
                       input_names=['input'],  # the model's input names
                       output_names=['output'],  # the model's output names
-                      dynamic_axes={'input': {0: 'batch_size', 1: 'audio_length'},  # variable length axes
-                                    'output': {0: 'batch_size', 1: 'audio_length'}})
+                      dynamic_axes={'input': {0: 'batch_size'},  # variable length axes
+                                    'output': {0: 'batch_size'}})
 
-    onnx_model = onnx.load("key-spotting.onnx")
+    onnx_model = onnx.load(output)
     onnx.checker.check_model(onnx_model)
